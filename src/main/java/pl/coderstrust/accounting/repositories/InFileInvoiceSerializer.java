8d8e83cbbe684517123d16b94d74542956d7c3ab
glk <52168046+mgrzelczyk@users.noreply.github.com>
2019-11-15 21:31:29 +0100

    Update serializer with new method

--- src/main/java/pl/coderstrust/accounting/repositories/InFileInvoiceSerializer.java
+++ src/main/java/pl/coderstrust/accounting/repositories/InFileInvoiceSerializer.java
@@ -5,12 +5,14 @@ import pl.coderstrust.accounting.model.Invoice;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InFileInvoiceSerializer {
 
-    private ArrayList<InFileInvoice> inFileInvoices = new ArrayList<>();
     private final FileHelper fileHelper;
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper;
     private Invoice invoice;
     private InFileInvoice inFileInvoice = new InFileInvoice(invoice, false);
 
@@ -18,9 +20,21 @@ public class InFileInvoiceSerializer {
         this.fileHelper = fileHelper;
     }
 
-    public String fromStrings () throws IOException {
+    public String toStrings () throws IOException {
         String inFilenvoiceJSON = objectMapper.writeValueAsString(inFileInvoice);
         fileHelper.writeLineToFile(inFilenvoiceJSON);
         return inFilenvoiceJSON;
     }
+
+    public Map<Long, InFileInvoice> fromStrings () throws IOException {
+        List<String> strings = fileHelper.readLinesFromFile();
+        Map<Long, InFileInvoice> database = new HashMap<>();
+        ArrayList<InFileInvoice> inFileInvoices = new ArrayList<>();
+        for (int i = 0; i < strings.size(); i++) {
+            inFileInvoices.add(objectMapper.readValue(strings.get(i), InFileInvoice.class));
+        }
+        inFileInvoices.forEach(inFileInvoice -> database.put(inFileInvoice.getId(), inFileInvoice));
+        return database;
+    }
+
 }

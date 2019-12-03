03ead003bdf7ea40e32fa320054f09f257ecd128
Filipx21 <filip.zajac2277@gmail.com>
2019-12-03 17:55:33 +0100

    Register in-hibernate-db

--- src/main/java/pl/coderstrust/accounting/config/AppConfiguration.java
+++ src/main/java/pl/coderstrust/accounting/config/AppConfiguration.java
@@ -1,8 +1,14 @@
 package pl.coderstrust.accounting.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
 import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
 import pl.coderstrust.accounting.repositories.HibernateDatabase;
 import pl.coderstrust.accounting.repositories.InMemoryDatabase;
@@ -21,5 +27,27 @@ public class AppConfiguration {
     public InvoiceDatabase inHibernateDatabase() {
         return new HibernateDatabase();
     }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule());
+    }
+
+    @Bean
+    public Formatter<LocalDate> localDateFormatter() {
+        return new Formatter<>() {
+            @Override
+            public LocalDate parse(String text, Locale locale) {
+                return LocalDate.parse(text, DateTimeFormatter.ISO_DATE);
+            }
+
+            @Override
+            public String print(LocalDate object, Locale locale) {
+                return DateTimeFormatter.ISO_DATE.format(object);
+            }
+        };
+    }
+
 
 }

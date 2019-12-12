c8d914edbca8cf10dca0eb1f8862e7a1591d291b
glk <52168046+mgrzelczyk@users.noreply.github.com>
2019-12-12 19:33:42 +0100

    Create logging task with pom update (log4j and slf4j properties)

--- src/main/java/pl/coderstrust/accounting/repositories/InMemoryDatabase.java
+++ src/main/java/pl/coderstrust/accounting/repositories/InMemoryDatabase.java
@@ -1,28 +1,35 @@
 package pl.coderstrust.accounting.repositories;
 
+import org.apache.log4j.Logger;
+import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
+import pl.coderstrust.accounting.model.Invoice;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
-import pl.coderstrust.accounting.model.Invoice;
 
 public class InMemoryDatabase implements InvoiceDatabase {
 
     private Map<Long, Invoice> invoiceMap = new ConcurrentHashMap<>();
     private AtomicLong counter = new AtomicLong(0);
+    private final static Logger LOGGER = Logger.getLogger(InMemoryDatabase.class);
 
     @Override
     public Invoice saveInvoice(Invoice invoice) {
+        LOGGER.info("Save invoice in Memory Database");
         Invoice copiedInvoice = new Invoice(invoice);
         if (copiedInvoice.getId() == null) {
+            LOGGER.info("Save invoice with null Id in Memory Database");
             Long id = counter.incrementAndGet();
             invoiceMap.put(id, copiedInvoice);
+            LOGGER.info("ID is set");
             copiedInvoice.setId(id);
             return copiedInvoice;
         } else if (invoiceMap.containsKey(copiedInvoice.getId())) {
+            LOGGER.info("Save invoice with Id in Memory Database");
             invoiceMap.put(copiedInvoice.getId(), copiedInvoice);
             return copiedInvoice;
         }
@@ -31,17 +38,20 @@ public class InMemoryDatabase implements InvoiceDatabase {
 
     @Override
     public Invoice findInvoiceById(Long id) {
+        LOGGER.info("Find Invoice by Id in Memory Database");
         return invoiceMap.get(id);
     }
 
     @Override
     public List<Invoice> findAllInvoices() {
+        LOGGER.info("Find All Invoices by Id in Memory Database");
         Collection<Invoice> values = invoiceMap.values();
         return new ArrayList<>(values);
     }
 
     @Override
     public Invoice deleteInvoiceById(Long id) {
+        LOGGER.info("Delete Invoice by Id in Memory Database");
         return invoiceMap.remove(id);
     }
 
--- src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
+++ src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
@@ -1,17 +1,20 @@
 package pl.coderstrust.accounting.services;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
 import pl.coderstrust.accounting.model.Invoice;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
 public class InvoiceBook {
 
     private final InvoiceDatabase invoiceDatabase;
+    private final static Logger LOGGER = Logger.getLogger(InvoiceBook.class);
 
     public InvoiceBook(InvoiceDatabase invoiceDatabase) {
         this.invoiceDatabase = invoiceDatabase;
@@ -19,16 +22,20 @@ public class InvoiceBook {
 
     public Invoice saveInvoice(Invoice invoice) {
         if (invoice != null) {
+            LOGGER.info("Save invoice in InvoiceBook services");
             return invoiceDatabase.saveInvoice(invoice);
         }
+        LOGGER.info("Null save invoice in InvoiceBook services");
         return null;
     }
 
     public Invoice findInvoiceById(Long id) {
         Invoice invoiceFound = invoiceDatabase.findInvoiceById(id);
         if (invoiceFound != null) {
+            LOGGER.info("Find Invoice by ID in InvoiceBook services");
             return invoiceFound;
         }
+        LOGGER.info("Find Invoice by ID is null in InvoiceBook services");
         return null;
     }
 
@@ -36,30 +43,37 @@ public class InvoiceBook {
         List<Invoice> invoices;
         invoices = new ArrayList<>(invoiceDatabase.findAllInvoices());
         if (!invoices.isEmpty()) {
+            LOGGER.info("Find all Invoices in InvoiceBook services");
             return invoices;
         }
+        LOGGER.info("Null invoices finded in InvoiceBook services");
         return null;
     }
 
     public List<Invoice> findAllInvoiceByDateRange(LocalDate from, LocalDate to) {
         List<Invoice> invoices = new ArrayList<>();
         for (Invoice invoice : invoiceDatabase.findAllInvoices()) {
+            LOGGER.info("Find all Invoice by data range in InvoiceBook services");
             if (invoice.getDate().isAfter(from) && invoice.getDate().isBefore(to)
                 || invoice.getDate().isEqual(from) || invoice.getDate().isEqual(to)) {
                 invoices.add(invoice);
             }
         }
         if (!invoices.isEmpty()) {
+            LOGGER.info("Finded Invoice by data range in InvoiceBook services");
             return invoices;
         }
+        LOGGER.info("Null finded Invoice by data range in InvoiceBook services");
         return null;
     }
 
     public Invoice deleteInvoiceById(Long id) {
         Invoice invoiceFound = invoiceDatabase.findInvoiceById(id);
         if (invoiceFound != null) {
+            LOGGER.info("Delete Invoice by ID in InvoiceBook services");
             return invoiceDatabase.deleteInvoiceById(id);
         }
+        LOGGER.info("Null deleted Invoice by ID in InvoiceBook services");
         return null;
     }
 

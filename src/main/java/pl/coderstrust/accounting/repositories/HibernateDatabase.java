7b992d79d3b983c77031b48d16ab23b0ff3ae43c
Filipx21 <filip.zajac2277@gmail.com>
2019-12-22 03:14:09 +0100

    Logback

--- src/main/java/pl/coderstrust/accounting/repositories/HibernateDatabase.java
+++ src/main/java/pl/coderstrust/accounting/repositories/HibernateDatabase.java
@@ -1,5 +1,7 @@
 package pl.coderstrust.accounting.repositories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
 import pl.coderstrust.accounting.mapper.InvoiceMapper;
@@ -20,16 +22,21 @@ public class HibernateDatabase implements InvoiceDatabase {
     @Autowired
     private InvoiceMapper invoiceMapper;
 
+    private final static Logger log = LoggerFactory.getLogger(HibernateDatabase.class);
+
     @Override
     public Invoice saveInvoice(Invoice invoice) {
         Invoice copiedInvoice = invoice;
         if (copiedInvoice.getId() == null && copiedInvoice.getDate() == null) {
+            log.warn("save - Empty invoice");
             throw new NullPointerException();
         } else if (copiedInvoice.getId() != null && copiedInvoice.getDate() == null) {
+            log.warn("save - Bad invoice fields");
             throw new NullPointerException();
         } else {
             InvoiceHib invoiceHib = invoiceMapper.toInvoiceHib(invoice);
             InvoiceHib saved = hibernateRepository.save(invoiceHib);
+            log.info("save - Invoice saved");
             return invoiceMapper.toInvoice(saved);
         }
     }
@@ -37,12 +44,14 @@ public class HibernateDatabase implements InvoiceDatabase {
     @Override
     public Invoice findInvoiceById(Long id) {
         Optional<InvoiceHib> object = hibernateRepository.findById(id);
+        log.info("Find invoice by id");
         return object.map(invoiceMapper::toInvoice).orElse(null);
     }
 
     @Override
     public List<Invoice> findAllInvoices() {
         Iterable<InvoiceHib> invoices = hibernateRepository.findAll();
+        log.info("Find all invoices");
         return StreamSupport
                 .stream(invoices.spliterator(), false)
                 .map(invoiceMapper::toInvoice)
@@ -54,8 +63,10 @@ public class HibernateDatabase implements InvoiceDatabase {
         Optional<InvoiceHib> object = hibernateRepository.findById(id);
         if (object.isPresent()) {
             hibernateRepository.deleteById(id);
+            log.info("delete - Invoice deleted");
             return invoiceMapper.toInvoice(object.get());
         }
+        log.warn("delete - Invoice doesn't exist");
         return null;
     }
 
--- src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
+++ src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
@@ -1,7 +1,5 @@
 package pl.coderstrust.accounting.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
 import pl.coderstrust.accounting.model.Invoice;

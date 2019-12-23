d5cb204ada49c2377b6945de42c1893496002cce
Filipx21 <filip.zajac2277@gmail.com>
2019-12-23 21:12:53 +0100

    Revert "Add logging"
    This reverts commit 348a808

--- src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
+++ src/main/java/pl/coderstrust/accounting/services/InvoiceBook.java
@@ -1,7 +1,5 @@
 package pl.coderstrust.accounting.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pl.coderstrust.accounting.infrastructure.InvoiceDatabase;
 import pl.coderstrust.accounting.model.Invoice;
@@ -15,28 +13,22 @@ public class InvoiceBook {
 
     private final InvoiceDatabase invoiceDatabase;
 
-    private final static Logger log = LoggerFactory.getLogger(InvoiceBook.class);
-
     public InvoiceBook(InvoiceDatabase invoiceDatabase) {
         this.invoiceDatabase = invoiceDatabase;
     }
 
     public Invoice saveInvoice(Invoice invoice) {
         if (invoice != null) {
-            log.info("[save] Invoice saved");
             return invoiceDatabase.saveInvoice(invoice);
         }
-        log.warn("[save] Incorrect data");
         return null;
     }
 
     public Invoice findInvoiceById(Long id) {
         Invoice invoiceFound = invoiceDatabase.findInvoiceById(id);
         if (invoiceFound != null) {
-            log.info("[findId] Invoice found");
             return invoiceFound;
         }
-        log.warn("[findId] Incorrect data or invoice doesn't exist");
         return null;
     }
 
@@ -44,10 +36,8 @@ public class InvoiceBook {
         List<Invoice> invoices;
         invoices = new ArrayList<>(invoiceDatabase.findAllInvoices());
         if (!invoices.isEmpty()) {
-            log.info("[findAll] Invoices found");
             return invoices;
         }
-        log.warn("[findAll] Invoices don't exist");
         return null;
     }
 
@@ -60,20 +50,16 @@ public class InvoiceBook {
             }
         }
         if (!invoices.isEmpty()) {
-            log.info("[findRange] Invoices found");
             return invoices;
         }
-        log.info("[findRange] Invoices don't exist");
         return null;
     }
 
     public Invoice deleteInvoiceById(Long id) {
         Invoice invoiceFound = invoiceDatabase.findInvoiceById(id);
         if (invoiceFound != null) {
-            log.info("[delete] Invoice deleted");
             return invoiceDatabase.deleteInvoiceById(id);
         }
-        log.warn("[delete] Invoice doesn't exist");
         return null;
     }
 
